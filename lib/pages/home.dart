d02315ce1fbe7009ac42bd197ee336269e873e7b
AttayR <attayrasoolofficial99@gmail.com>
2024-08-28 13:21:25 +0500

    apply changes and update the app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:news_updates/models/article_model.dart';
 import 'package:news_updates/models/category_models.dart';
 import 'package:news_updates/models/slider_modal.dart';
+import 'package:news_updates/pages/article_view.dart';
 import 'package:news_updates/services/data.dart';
 import 'package:news_updates/services/news.dart';
 import 'package:news_updates/services/slider_data.dart';
@@ -21,23 +22,30 @@ class _HomeState extends State<Home> {
   List<sliderModal> sliders = [];
   List<ArticleModel> articles = [];
   bool _loading = true;
-
   int activeIndex = 0;
+
   @override
   void initState() {
-    categories = getCategories();
-    sliders = getSlider();
     super.initState();
-
+    categories = getCategories();
+    getSlider();
     getNews();
   }
 
-  getNews() async {
+  Future<void> getNews() async {
     News newsclass = News();
     await newsclass.getNews();
-    articles = newsclass.news;
     setState(() {
+      articles = newsclass.news;
       _loading = false;
+    });
+  }
+
+  Future<void> getSlider() async {
+    Sliders slider = Sliders();
+    await slider.getSlider();
+    setState(() {
+      sliders = slider.sliders;
     });
   }
 
@@ -52,8 +60,7 @@ class _HomeState extends State<Home> {
             Text("News"),
             Text(
               "Feed",
-              style:
-                  TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -63,120 +70,129 @@ class _HomeState extends State<Home> {
       body: _loading
           ? Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 10.0),
-                      height: 70,
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: categories.length,
-                        itemBuilder: (context, index) {
-                          return CategoryTiles(
-                            image: categories[index].image,
-                            categoryName: categories[index].categoryName,
-                          );
-                        },
-                      ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    height: 70,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return CategoryTiles(
+                          image: categories[index].image,
+                          categoryName: categories[index].categoryName,
+                        );
+                      },
                     ),
-                    SizedBox(
-                      height: 30.0,
+                  ),
+                  SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Breaking News!",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0),
+                        ),
+                        Text(
+                          "View All",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Breaking News!",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0)),
-                          Text("View All",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16.0)),
-                        ],
-                      ),
+                  ),
+                  SizedBox(height: 30.0),
+                  CarouselSlider.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index, realIndex) {
+                      String? image = sliders[index].urlToImage;
+                      String? title = sliders[index].title;
+                      return buildImage(image ?? '', index, title ?? '');
+                    },
+                    options: CarouselOptions(
+                      height: 250,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          activeIndex = index;
+                        });
+                      },
                     ),
-                    SizedBox(
-                      height: 30.0,
+                  ),
+                  SizedBox(height: 30.0),
+                  Center(child: buildIndicator()),
+                  SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Trending News!",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0),
+                        ),
+                        Text(
+                          "View All",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0),
+                        ),
+                      ],
                     ),
-                    CarouselSlider.builder(
-                        itemCount: sliders.length,
-                        itemBuilder: (context, index, realIndex) {
-                          String? res = sliders[index].image;
-                          String? res2 = sliders[index].name;
-                          return builImage(res!, index, res2!);
-                        },
-                        options: CarouselOptions(
-                            height: 250,
-                            autoPlay: true,
-                            enlargeCenterPage: true,
-                            enlargeStrategy: CenterPageEnlargeStrategy.height,
-                            onPageChanged: (index, reason) {
-                              setState(() {
-                                activeIndex = index;
-                              });
-                            })),
-                    SizedBox(height: 30.0),
-                    Center(child: buildIndicator()),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Trending News!",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0)),
-                          Text("View All",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16.0)),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                        child: ListView.builder(
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    child: ListView.builder(
                       shrinkWrap: true,
                       physics: ClampingScrollPhysics(),
                       itemCount: articles.length,
                       itemBuilder: (context, index) {
                         return BlogTile(
-                            imageUrl: articles[index].urlToImage!,
-                            title: articles[index].title!,
-                            desc: articles[index].description!);
+                          author: articles[index].author ?? 'Unknown',
+                          url: articles[index].url ?? '',
+                          imageUrl: articles[index].urlToImage ?? '',
+                          title: articles[index].title ?? 'No Title',
+                          desc: articles[index].description ?? 'No Description',
+                        );
                       },
-                    )),
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
             ),
     );
   }
 
-  Widget builImage(String image, int index, String name) => Container(
-      margin: EdgeInsets.symmetric(horizontal: 5.0),
-      child: Stack(children: [
+  Widget buildImage(String image, int index, String name) => Container(
+    margin: EdgeInsets.symmetric(horizontal: 5.0),
+    child: Stack(
+      children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Image.asset(image,
-              fit: BoxFit.cover,
-              height: 250,
-              width: MediaQuery.of(context).size.width),
+          child: CachedNetworkImage(
+            imageUrl: image,
+            fit: BoxFit.cover,
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
         ),
         Container(
           height: 250,
@@ -184,24 +200,31 @@ class _HomeState extends State<Home> {
           margin: EdgeInsets.only(top: 170.0),
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-              color: Colors.black26,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20))),
+            color: Colors.black26,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
           child: Text(
             name,
+            maxLines: 2,
             style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold),
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ]));
+      ],
+    ),
+  );
+
   Widget buildIndicator() => AnimatedSmoothIndicator(
-        activeIndex: activeIndex,
-        count: sliders.length,
-        effect: ExpandingDotsEffect(activeDotColor: Colors.green),
-      );
+    activeIndex: activeIndex,
+    count: 5,
+    effect: ExpandingDotsEffect(activeDotColor: Colors.green),
+  );
 }
 
 class CategoryTiles extends StatelessWidget {
@@ -219,20 +242,31 @@ class CategoryTiles extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(6),
-            child:
-                Image.asset(image!, width: 120, height: 60, fit: BoxFit.cover),
+            child: Image.asset(
+              image ?? '',
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) => Icon(Icons.error),
+            ),
           ),
           Container(
             width: 120,
             height: 60,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6), color: Colors.black38),
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black38,
+            ),
             child: Center(
-                child: Text(categoryName!,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold))),
+              child: Text(
+                categoryName ?? 'Category',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -241,15 +275,26 @@ class CategoryTiles extends StatelessWidget {
 }
 
 class BlogTile extends StatelessWidget {
-  String imageUrl, title, desc;
-  BlogTile({required this.imageUrl, required this.title, required this.desc});
-  @override
+  final String imageUrl, title, desc, url, author;
+
+  BlogTile({
+    required this.imageUrl,
+    required this.title,
+    required this.desc,
+    required this.url,
+    required this.author,
+  });
+
+@override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // Define the action to perform on tap
-        print('Padding tapped');
-        // You can navigate to another screen or perform any other action here
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ArticleView(blogUrl: url),
+          ),
+        );
       },
       child: Container(
         margin: EdgeInsetsDirectional.only(bottom: 10),
@@ -258,54 +303,60 @@ class BlogTile extends StatelessWidget {
           child: Material(
             elevation: 3.0,
             borderRadius: BorderRadius.circular(10),
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: CachedNetworkImage(
-                        imageUrl: imageUrl,
-                        height: 150,
-                        width: 150,
-                        fit: BoxFit.cover,
-                      ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
                     ),
                   ),
-                  SizedBox(width: 8.0),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.8,
-                        child: Text(
-                          maxLines: 2,
-                          title,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16.0),
-                        ),
-                      ),
-                      SizedBox(height: 9.0),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.8,
-                        child: Text(
-                          maxLines: 3,
-                          desc,
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.w300,
-                              fontSize: 16.0),
-                        ),
-                      ),
-                    ],
+                ),
+                SizedBox(height: 10.0), 
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    desc,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 14.0,
+                    ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 20.0), 
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    'Author: $author',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12.0,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -21,25 +21,34 @@ class _LandingPageState extends State<LandingPage> {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(30),
                 child: Image.asset(
-                  "images/building2.jpg",
+                  "images/building4.jpg",
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height / 1.5,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
-            const SizedBox(height: 30.0),
+           const SizedBox(height: 30.0),
             const Text(
-              "News from around the\nworld for you",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              "Stay Informed with the Latest News",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                letterSpacing: 1.2,
+              ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 30.0),
+            const SizedBox(height: 20.0),
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 20.0),
               child: const Text(
-                "News from around the world for you, so take your time to read",
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400),
+                "Explore the most recent updates and in-depth stories from around the globe. Your next favorite article is just a tap away.",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
                 textAlign: TextAlign.center,
               ),
             ),
